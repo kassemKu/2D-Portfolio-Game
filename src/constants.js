49dc9cbc51d5daf684b265ed536bef3579c19119
kassemKu <arabisc95@gmail.com>
2024-11-05 07:43:16 +0200

    first commit

---
+++ src/constants.js
@@ -0,0 +1,16 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+  pc: `This is my PC. I work mostly in JavaScript/Tailwind these days.
+    I've made a couple of games in that language.
+    Here is my <a href="https://github.com/kassemKu" target="_blank">Github</a>!`,
+  "cs-degree": `This is my CS degree. I hung it on the wall because I'm proud of it. It was a very theoretical degree but I think it gave me a good foundation.`,
+  "sofa-table": `That's my sofa. I like to relax here and watch YouTube, or playing Clash Royal.`,
+  tv: `That's my TV. I don't watch it a lot`,
+  bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
+  resume: `This is my desk. I often work in programming on it. If you want to see my projects <a href="https://vercel.com/kassems-projects-d6fba6af" target="_blank">Vercel</a>!`,
+  projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
+  Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas..`,
+  library: `The books of school :)`,
+  exit: `If you want to exit Drdeveloper's portfolio, just close the tab.`,
+};
